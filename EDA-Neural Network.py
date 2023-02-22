65068cb6a7199e97fa838192980cbe4c0388f7db
DiogoAmaro13 <122986065+DiogoAmaro13@users.noreply.github.com>
2023-02-22 16:48:49 +0000

    Update EDA-Neural Network.py

--- EDA-Neural Network.py
+++ EDA-Neural Network.py
@@ -1,7 +1,5 @@
 # 0. Import and Install Dependencies
-
 %matplotlib inline
-
 import os
 import pandas as pd
 import seaborn as sns
@@ -11,8 +9,8 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 
-### Install tensorflow
+# 0.1 Install tensorflow
 !pip install tensorflow
 
-### Check if the dependencies are installed
+# 0.2 Check if the dependencies are installed
 !pip list
