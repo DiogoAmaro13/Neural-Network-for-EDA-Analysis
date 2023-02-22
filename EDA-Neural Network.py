8c196d73857db65575d5e14b4ddb289089c0fd77
DiogoAmaro13 <122986065+DiogoAmaro13@users.noreply.github.com>
2023-02-22 17:19:43 +0000

    Update EDA-Neural Network.py

--- EDA-Neural Network.py
+++ EDA-Neural Network.py
@@ -1,9 +1,9 @@
 # 0. Import and Install Dependencies
 
 %matplotlib inline
-import os
-import pandas as pd
-import seaborn as sns
+import os  
+import pandas as pd  
+import seaborn as sns   
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import mutual_info_classif
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -15,7 +15,6 @@ import numpy as np
 
 # 0.2 Check if the dependencies are installed
 !pip list
-
 
 
 # 1. Load and Study Data 
@@ -30,9 +29,8 @@ file_path
 df = pd.read_csv('C:\\Users\\jmsi\\emotions.csv')
 
 # 1.2 Study the data
-""" To study the data, it is recommend to use methods that allow us to find general info about our dataset, such as:"""
-
-  #✔ Was the dataset loaded correctly into our environment?"""
+""" To study the data, it is recommend to use methods that allow us to find general info about our dataset, such as: """
+  #✔ Was the dataset loaded correctly into our environment?
   df
   #✔ General view on the dataframe
   df.head()
@@ -46,7 +44,22 @@ df = pd.read_csv('C:\\Users\\jmsi\\emotions.csv')
   for col in df.columns:
     if df[col].isnull().sum()>0:
         print(col)
-
+  #✔ How many categorical features are in our dataset?
+  for col in df.columns:
+    if(df[col].dtype=='object'):
+        print(col)
+        
+""" Take the conclusions: """
+  #✔ Was the dataset loaded correctly into our environment?
+    # Yes it was, because 'df' displays an actual dataframe, which means the load was done properly
+  #✔ General view on the dataframe
+    # With 'df.head()' we get the first 5 rows of our dataset
+  #✔ Number of rows & columns
+    # 'df.shape()' gives us the shape of our dataset -> (2132, 2549)
+  #✔ Statistical values
+    # 'df.describe()' outputs a dataframe with values such as mean, standard deviation, min, max, etc
+  #✔ Are the values in our dataset balanced?
+    # The code provided allow us to understant
 
 
   
